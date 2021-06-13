14ec6ac436925a7fd519a96d8e2ff8777e2eee0c
DaimSyed <daimshah784@gmail.com>
2021-06-14 01:39:32 +0500

    added

---
+++ build/precache-manifest.9682dd9e7c4a6bdaec4647d29a051632.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60cf560980334a0056e559cc7cef9d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "984e3dad779d62adfdaf",
+    "url": "/static/js/2.989481ef.chunk.js"
+  },
+  {
+    "revision": "9c27462deed2de34a263",
+    "url": "/static/js/main.fc6ca140.chunk.js"
+  },
+  {
+    "revision": "583e51a7281672351041",
+    "url": "/static/js/runtime-main.38f647cd.js"
+  }
+]);
